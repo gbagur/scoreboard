05a942d935302aebd6fbc87055163c3a3b23f639
Gabriel Bagur <gbagur@ant-neuro.com>
2024-11-23 16:57:57 +0100

    Step motor test

--- Arduino/StepMotorTest/StepMotorTest.ino
+++ Arduino/StepMotorTest/StepMotorTest.ino
@@ -1,9 +1,10 @@
 // Define step and direction pins
 const int stepPin = 13;
 const int dirPin = 12;
+const int enPin = 32;
 
 // Define the steps per revolution for your stepper motor
-const int stepsPerRevolution = 200;  // Adjust this value according to your motor specifications
+const int stepsPerRevolution = 3200;  // Adjust this value according to your motor specifications
 
 // UART buffer
 const int BUFFER_SIZE = 50; // Define a buffer size for incoming serial data
@@ -17,10 +18,13 @@ void setup() {
   // Set the step and direction pins as outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+ 
 
   // Set the initial direction
   digitalWrite(dirPin, HIGH);  // Set direction to clockwise
   digitalWrite(stepPin, HIGH);
+  digitalWrite(enPin, HIGH);
 
   Serial.begin(115200);
   Serial.print ("Test de motor y sensor de proximidad.");
@@ -34,11 +38,24 @@ void loop() {
     char command = Serial.read(); // Read the incoming command
     Serial.print("Commando recibido: ");
     Serial.println(command);
-    if (command == 'a') {
-      stepMotor(CLOCKWISE,36,1);
+    int speed = 5;
+    if (command == 'p') {
+      stepMotor(CLOCKWISE,36,speed);
     }
-    if (command == 'b') {
-      stepMotor(ANTICLOCKWISE,36,1);
+    if (command == 'o') {
+      stepMotor(ANTICLOCKWISE,36,speed);
+    }
+    if (command == 'c') {
+      stepMotor(ANTICLOCKWISE,360,speed);
+    }
+    if (command == 'l') {
+      stepMotor(CLOCKWISE,2,speed);
+    }
+    if (command == 'k') {
+      stepMotor(ANTICLOCKWISE,2,speed);
+    }    
+    if (command == 'i') {
+      digitalWrite(enPin, LOW);
     }
 
   } 
@@ -46,6 +63,7 @@ void loop() {
 
 // arg direction 0=anticlockise 1=clockwise
 void stepMotor (int direction, int angle, int speed) {
+  digitalWrite(enPin, LOW);
   digitalWrite(dirPin, direction);
   int steps = (stepsPerRevolution * angle) / 360;
   Serial.printf("StepMotor: dir=%d, angle=%d, speed=%d, steps=%d\r\n", direction, angle, speed, steps);
@@ -54,9 +72,10 @@ void stepMotor (int direction, int angle, int speed) {
     // Generate a step pulse
     digitalWrite(stepPin, HIGH);
     //digitalWrite(LED_BUILTIN, HIGH);
-    delayMicroseconds(1000/speed);  // Adjust delay for desired speed
+    delayMicroseconds(1000*speed);  // Adjust delay for desired speed
     digitalWrite(stepPin, LOW);
     //digitalWrite(LED_BUILTIN, LOW);
-    delayMicroseconds(1000/speed);  // Adjust delay for desired speed
+    delayMicroseconds(1000*speed);  // Adjust delay for desired speed
   }
+  digitalWrite(enPin, HIGH);
 }
